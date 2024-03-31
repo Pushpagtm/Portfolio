cea46f7b9e511634eebde4f09ede9be8b9dcee68
Pushpagtm <pushpagautam529@gmail.com>
2024-03-31 10:47:21 +0545

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import Header from '../components/Header/Header'
-import Aboutpage from '../components/Aboutpage/Aboutpage'
-import { Footer } from '../components/Footer'
-
+import React from "react";
+import Header from "../components/Header/Header";
+import Aboutpage from "../components/Aboutpage/Aboutpage";
+import { Footer } from "../components/Footer";
 
 function About() {
   return (
     <div>
       <Header />
       <Aboutpage />
-      {/* <Footer/> */}
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import Header from '../components/Header/Header'
-import Projectlist from '../components/Projectlist/Projectlist'
-import { Footer } from '../components/Footer'
+import React from "react";
+import Header from "../components/Header/Header";
+import Projectlist from "../components/Projectlist/Projectlist";
+import { Footer } from "../components/Footer";
 
 function Project() {
   return (
     <div>
       <Header />
       <Projectlist />
-      {/* <Footer/> */}
     </div>
-  )
+  );
 }
 
-export default Project
+export default Project;
